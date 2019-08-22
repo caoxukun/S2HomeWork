64f026c40201ca20dc68b45ab928ce1c517b3666
caoxukun <2821922417@qq.com>
2019-08-22 22:02:02 +0800

    第二章第五题

---
+++ HomeWork/src/s2homework/Animal.java
@@ -0,0 +1,8 @@
+package s2homework;
+
+public abstract class Animal {
+    String color;
+    String weight;
+    int age;
+    public abstract void printInfo();
+}
---
+++ HomeWork/src/s2homework/Fish.java
@@ -0,0 +1,13 @@
+package s2homework;
+
+public class Fish extends Animal {
+    Fish(String weight, int age) {
+        super.weight = weight;
+        super.age = age;
+    }
+    @Override
+    public void printInfo() {
+        System.out.println("我是一条" + weight + "斤重的鱼！");
+        System.out.println("今年" + age + "岁了！");
+    }
+}
