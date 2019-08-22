a65a6cf4c8e62a3367c046bf78e55aece02c23ed
caoxukun <2821922417@qq.com>
2019-08-22 22:18:20 +0800

    第二章第六题

---
+++ HomeWork/src/s2homework/huik/Bird.java
@@ -0,0 +1,14 @@
+package s2homework.huik;
+
+public class Bird extends Animal {
+    Bird(String color, int age) {
+        super.color = color;
+        super.age = age;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("我是一只" + color + "的鸟！");
+        System.out.println("今年" + age + "岁了！\n");
+    }
+}
---
+++ HomeWork/src/s2homework/huik/Test.java
@@ -0,0 +1,10 @@
+package s2homework.huik;
+
+public class Test {
+    public static void main(String[] args) {
+        Bird b = new Bird("红色", 4);
+        Fish f = new Fish("5", 2);
+        b.printInfo();
+        f.printInfo();
+    }
+}
